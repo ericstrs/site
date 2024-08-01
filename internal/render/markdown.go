bf0ce965047c01f588224d9e98d78e3dc38250a1
ericstrs <98285990+ericstrs@users.noreply.github.com>
2024-07-31 22:31:14 -0500

    feat: set background and muted color and center code blocks

--- internal/render/markdown.go
+++ internal/render/markdown.go
@@ -22,6 +22,7 @@ func markdownToHTML(content []byte) ([]byte, error) {
 				highlighting.WithStyle("gruvbox"),
 				highlighting.WithFormatOptions(
 					chromahtml.WithLineNumbers(true),
+					chromahtml.WrapLongLines(true),
 				),
 			),
 		),
